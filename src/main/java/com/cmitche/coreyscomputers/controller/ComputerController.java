ae592eca8a70508e67cab2c91acd752d900a69f0
James Costner <jamesmp9957@outlook.com>
2022-09-07 20:51:29 -0500

    added get all filter by sku and filter by type in the restcontroller

--- src/main/java/com/cmitche/coreyscomputers/controller/ComputerController.java
+++ src/main/java/com/cmitche/coreyscomputers/controller/ComputerController.java
@@ -6,6 +6,7 @@ import com.cmitche.coreyscomputers.vo.Computer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -19,14 +20,34 @@ public class ComputerController {
           return computerService.findAllComputers();
       }
 
-    // FILTER ITEMS BASED ON SKU NUMBER
+    //filter by sku number
+    public List<Computer> filterBySkuNumber(Long skuNumber){
+          List<Computer> computers = computerService.findAllComputers();
+          List<Computer> filteredComputers = new ArrayList<>();
+          for (Computer computer : computers) {
+              if (computer.getSkuNumber().equals(skuNumber)) {
+                  filteredComputers.add(computer);
+              }
+          }
+              return filteredComputers;
+          }
+
 
     // FILTER ITEMS BASED ON PRODUCT TYPE
+    public List<Computer> filterByProductType(String type){
+        List<Computer> computers = computerService.findAllComputers();
+        List<Computer> filteredComputers = new ArrayList<>();
+        for (Computer computer : computers) {
+            if (computer.getType().equals(type)) {
+                filteredComputers.add(computer);
+            }
+        }
+        return filteredComputers;
+    }
 
     // FILTER ITEMS BASED ON PRICE
 
     // FILTER ITEMS BASED ON BRAND
-
     // FILTER ITEMS BASED ON STAR RATING
 
 }
